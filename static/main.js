f972251e5e729bf303c444d75a4a9b2c1c25df0e
juliedosher <juliegdosher@lewisu.edu>
2023-03-07 22:47:45 -0600

    merged main.js with script.js

--- static/main.js
+++
@@ -1,9 +0,0 @@
-async function start() {
-    const response = await fetch("http://localhost:3000") // for testing on local machine    TODO: remove later
-   // const response = await fetch("https://dps-insuranceriskcalculator-server.azurewebsites.net") // for running with web server
-    const data = await response.json()
-    console.log(data.message())
-    
-}
-
-start()
