aaa13b41f2dc317bd40c1ff5a0dc2f405470c0b0
Julie Dosher <112606564+juliedosher@users.noreply.github.com>
2023-03-10 18:27:09 -0600

    added getBMIPoints()

--- static/script.js
+++ static/script.js
@@ -1,32 +1,56 @@
 const url = "https://dps-insuranceriskcalculator-server.azurewebsites.net";
 
 async function start() {
-    //const response = await fetch("http://localhost:3000") // for testing on local machine    TODO: remove later
-    const response = await fetch(url + "/ping") // for running with web server
-    const data = await response.json()
-    console.log(data.message())
+    //const response = await fetch("http://localhost:3000"); // for testing on local machine    TODO: remove later
+    const response = await fetch(url + "/api/ping"); // for running with web server
+    const data = await response.json();
+    console.log(data.message());
     
 }
 
-start()
-
-async function agePoints() {
-    url = url + "/calc-age";    
+async function getAgePoints() {
+    currURL = url + "/api/calc-age";    
     
     var data = document.getElementById("age").value;
 
-    let request = new Request (url, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-});
-    const res = await fetch(request)
-    const result = await res.json()
-    document.getElementById("age-points").innerHTML = result
-    console.log(result)
+    let request = new Request (currURL, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    });
+    const res = await fetch(request);
+    const result = await res.json();
+    document.getElementById("age-points").innerHTML = result;
+    console.log(result);
 }
 
-agePoints();
+async function getBMIPoints() {
+    currURL = url + "/api/calc-bmi";
+    
+    var data = document.getElementById("bmi").value;
+    let request = new Request(currURL, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    });
+    const res = await fetch(request);
+    const result = await res.json();
+    document.getElementById("bmi-points").innerHTML = result;
+    console.log(result);
+}
+
+
+function calculateRisk() {
+    getAgePoints();
+    getBMIPoints();
+}
+    
+
+start();
+calculateRisk();
