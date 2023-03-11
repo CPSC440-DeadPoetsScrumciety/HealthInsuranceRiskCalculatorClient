c977561a62f743a066ae3b9c09e6cc73cecc37f2
Vy Nguyen Khanh Le <vykhanhle9971@gmail.com>
2023-03-11 00:25:57 -0600

    Added changes

--- static/script.js
+++ static/script.js
@@ -1,25 +1,24 @@
-const url = "https://dps-insuranceriskcalculator-server.azurewebsites.net";
+const apiUrl = "https://dps-insuranceriskcalculator-server.azurewebsites.net";
 
 async function start() {
     //const response = await fetch("http://localhost:3000") // for testing on local machine    TODO: remove later
-    const response = await fetch(url + "/ping") // for running with web server
+    const response = await fetch(apiUrl + "/ping") // for running with web server
     const data = await response.json()
     console.log(data.message())
-    
 }
 
 start()
 
 async function agePoints() {
-    url = url + "/calc-age";    
+    var url = apiUrl + "/calc-age";    
     
     var data = document.getElementById("age").value;
 
     let request = new Request (url, {
+    mode: "no-cors",
     method: "POST",
     body: JSON.stringify(data),
     headers: {
-        "Accept": "application/json",
         "Content-Type": "application/json"
     }
 });
